830860a8886ec71639bcc6307d5e37bd4116063e
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-23 22:29:58 +1100

    Add vowels

--- vowels_and_consonants_counter/Program.cs
+++ vowels_and_consonants_counter/Program.cs
@@ -19,6 +19,17 @@ namespace vowels_and_consonants_counter
             {
                 sentence[i] = Convert.ToChar(Console.Read());
             }
+
+            for (i = 0; sentence[i] != '\0'; i++)
+            {
+                if ((sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] ==
+                'i' || sentence[i] == 'o' || sentence[i] == 'u') ||
+                (sentence[i] == 'A' || sentence[i] == 'E' || sentence[i] ==
+                'I' || sentence[i] == 'O' || sentence[i] == 'U'))
+                {
+                    vowels = vowels + 1;
+                }
+            }
         }
     }
 }
